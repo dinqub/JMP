d01b6abb34e382793010f816020c9c3df163bb7c
Dzmitry Mukha1 <dzmitry_mukha1@epam.com>
2017-06-27 01:51:08 +0300

    final changes

--- Module_MongoDB/src/main/java/epam/jmp/muha/NoteBook.java
+++ Module_MongoDB/src/main/java/epam/jmp/muha/NoteBook.java
@@ -1,17 +1,25 @@
 package epam.jmp.muha;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 
-
 public class NoteBook {
-	public static void main(String[] args) {
+	private static final String EXIT = "exit";
+	private static final String YES = "y";
+
+	public static void main(String[] args) throws IOException {
 
 		try {
 
@@ -23,44 +31,152 @@ public class NoteBook {
 
 			/**** Get collection / table from 'testdb' ****/
 			DBCollection table = db.getCollection("user");
-
-			/**** Insert ****/
-			BasicDBObject document = new BasicDBObject();
-			document.put("name", "dima");
-			document.put("age", 30);
-			document.put("createdDate", new Date());
-			table.insert(document);
-
-			/**** Find and display ****/
-			BasicDBObject searchQuery = new BasicDBObject();
-			searchQuery.put("name", "dima");
-
-			DBCursor cursor = table.find(searchQuery);
-
-			while (cursor.hasNext()) {
-				System.out.println(cursor.next());
-			}
-
-			/**** Update ****/
-			BasicDBObject query = new BasicDBObject();
-			query.put("name", "dima");
-
-			BasicDBObject newDocument = new BasicDBObject();
-			newDocument.put("name", "dima-updated");
-
-			BasicDBObject updateObj = new BasicDBObject();
-			updateObj.put("$set", newDocument);
-
-			table.update(query, updateObj);
-
-			/**** Find and display ****/
-			BasicDBObject searchQuery2 
-				= new BasicDBObject().append("name", "dima-updated");
-
-			DBCursor cursor2 = table.find(searchQuery2);
-
-			while (cursor2.hasNext()) {
-				System.out.println(cursor2.next());
+			Scanner in = new Scanner(System.in);
+			String choose = null;
+			while (!EXIT.equals(choose))
+			{
+				System.out.println("Print 1 to view all records," + 
+						"\nPrint 2 to insert new," + 
+						"\nPrint 3 to find by tag,"+
+						"\nPrint 4 to find by tag and note"+
+						"\nPrint exit for exit");
+				choose = in.nextLine();				
+				switch (choose) {
+				case "1":
+					System.out.println("Show all records");
+					/**** Find all ****/
+					DBCursor cursorAll = table.find();
+	
+					boolean existResultAll = cursorAll.hasNext();
+					if(!existResultAll)
+					{
+						System.out.println("Nothing found");
+					}
+					else
+					{
+						System.out.println("Result:");
+					}
+	
+					List<DBObject> resultListAll = new ArrayList<DBObject>();
+					while (cursorAll.hasNext())
+					{
+						DBObject dbDocument = cursorAll.next();
+						System.out.println(dbDocument);
+						resultListAll.add(dbDocument);
+					}
+					if(existResultAll)
+					{
+						System.out.println("Do you want remove it (y/n)");
+						String result = in.nextLine();	
+						if (YES.equals(result))
+						{
+							for (DBObject dbObject : resultListAll)
+							{
+								table.remove(dbObject);
+							}							
+						}
+					}	
+					break;
+				case "2":
+					/**** Insert ****/				
+					System.out.println("Enter tag:\n");
+					String tag = in.nextLine();	
+					System.out.println("Enter note:");
+					String note = in.nextLine();	
+					
+					BasicDBObject document = new BasicDBObject();
+					document.put("date", new Date());
+					document.put("tag", tag);	
+					document.put("note", note);
+					table.insert(document);
+					System.out.println("Inserted" + document);
+					break;
+				case "3":
+					System.out.println("Finded by tag");
+					/**** Find and display ****/
+					System.out.println("Enter tag:\n");
+					String tagName = in.nextLine();	
+					BasicDBObject searchTagQuery = new BasicDBObject();
+					searchTagQuery.put("tag", tagName);
+	
+					DBCursor cursorTag = table.find(searchTagQuery);
+					boolean existTagResult = cursorTag.hasNext();
+					if(!existTagResult)
+					{
+						System.out.println("Nothing found");
+					}
+					else
+					{
+						System.out.println("Result:");
+					}
+	
+					List<DBObject> resultTagList = new ArrayList<DBObject>();
+					while (cursorTag.hasNext())
+					{
+						DBObject dbDocument = cursorTag.next();
+						System.out.println(dbDocument);
+						resultTagList.add(dbDocument);
+					}
+					if(existTagResult)
+					{
+						System.out.println("Do you want remove it (y/n)");
+						String result = in.nextLine();	
+						if (YES.equals(result))
+						{
+							for (DBObject dbObject : resultTagList)
+							{
+								table.remove(dbObject);
+							}							
+						}
+					}
+					break;
+				case "4":
+					System.out.println("Finded by tag and note");
+					/**** Find and display ****/
+					System.out.println("Enter tag:\n");
+					String tagParameter = in.nextLine();	
+					System.out.println("Enter note:\n");
+					String noteParameter = in.nextLine();	
+					
+					BasicDBObject searchQuery = new BasicDBObject();
+					searchQuery.put("tag", tagParameter);
+					searchQuery.put("note",noteParameter);
+	
+					DBCursor cursor = table.find(searchQuery);
+					
+					boolean existResult = cursor.hasNext();
+					if(!existResult)
+					{
+						System.out.println("Nothing found");
+					}
+					else
+					{
+						System.out.println("Result:");
+					}
+	
+					List<DBObject> resultList = new ArrayList<DBObject>();
+					while (cursor.hasNext())
+					{
+						DBObject dbDocument = cursor.next();
+						System.out.println(dbDocument);
+						resultList.add(dbDocument);
+					}
+					if(existResult)
+					{
+						System.out.println("Do you want remove it (y/n)");
+						String result = in.nextLine();	
+						if (YES.equals(result))
+						{
+							for (DBObject dbObject : resultList)
+							{
+								table.remove(dbObject);
+							}							
+						}
+					}					
+					break;
+				default:
+					break;
+				}
 			}
 
 			/**** Done ****/
